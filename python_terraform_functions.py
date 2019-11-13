4631da3d980751aed94935a7b3ba664ca2fad69f
Rohit Gottiparthy <rohitgottiparthy@gmail.com>
2019-11-13 11:31:12 -0800

    python_terraform_functions.py

---
+++ python_terraform_functions.py
@@ -0,0 +1,42 @@
+from python_terraform import *
+
+class TerraformWrapper(object):
+    ''' Terraform basic wrapper '''
+
+    def __init__(self, working_dir):
+        ''' Directory path of tf files '''
+
+        self.working_dir = working_dir
+
+    def execute(self, action):
+        ''' Execute action to run. Example init, validate, apply '''
+
+        print('Running terraform')
+        print action
+        tef = Terraform(self.working_dir)
+        if action == "init":
+            tef.init(reconfigure=True)
+            return_code, stdout, stderr = tef.init()
+            return [return_code, stdout, stderr]
+        elif action == "validate":
+            tef.validate()
+            return_code, stdout, stderr = tef.validate()
+            return [return_code, stdout, stderr]
+        elif action == "plan":
+            tef.plan(refresh=False, capture_output=True)
+            return_code, stdout, stderr = tef.plan()
+            return [return_code, stdout, stderr]
+        elif action == "apply":
+            return_code, stdout, stderr = tef.apply("-auto-approve", capture_output=True)
+            return [return_code, stdout, stderr]
+        elif action == "destroy":
+            return_code, stdout, stderr = tef.destroy("-auto-approve", capture_output=True)
+            return [return_code, stdout, stderr]
+        print ('Unsupported option was passed')
+        print action
+        return False
+        
+        
+        #Sample usage:
+        #tf = tfg.Terraform_wrapper('/home/rgottiparthy/terraform/tfs')
+        #res = tf.execute('validate')
